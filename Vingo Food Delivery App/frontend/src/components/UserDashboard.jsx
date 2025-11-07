dde5059c0985df3aa673697b319b64fde0c64976
prajwal21ambig45-lang <prajwal21ambig45@gmail.com>
2025-11-07 13:53:20 +0530

    Update UserDashboard.jsx

--- Vingo Food Delivery App/frontend/src/components/UserDashboard.jsx
+++ Vingo Food Delivery App/frontend/src/components/UserDashboard.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from 'react'
-import UserDashboard from "../components/UserDashboard";
 import Nav from './NaV.JSX'
 import { categories } from '../category'
 import CategoryCard from './CategoryCard'
